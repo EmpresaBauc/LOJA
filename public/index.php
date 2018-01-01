da57e7af69becf4688ea9c4c4d7ecc9b317365cc
lucas <de.lucas73@gmail.com>
2018-01-01 21:13:01 -0200

    loja terminada

---
+++ public/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewprt" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Loja Avanção</title>
+  <link rel="stylesheet" href="../libs/normalize/css/normalize-7.0.0.css">
+  <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../libs/css/loja.css">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="textotitulo">LOJA AVANÇÃO</h1>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					  	<div class="text-center">
+					  		<img class="imgproduto" src="../assets/imagens/acai1.jpg">
+					   		<h4 class="produto">Açai 300ml</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$100</text>
+					   	 </div> 	
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/acai2.jpg">				  	
+					   		<h4 class="produto">Açai 500ml</h4>
+							<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$150</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/ventilador1.jpg">				  	
+					   		<h4 class="produto">Ventilador de Mesa</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$500</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+					   		<img class="imgproduto" src="../assets/imagens/pendrive1.jpg">
+					   		<h4 class="produto">Pendrive 64GB</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$500</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/manicure1.jpg">				  	
+					   		<h4 class="produto">Manicure</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$400</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+					   		<img class="imgproduto" src="../assets/imagens/cinema1.jpg">
+					   		<h4 class="produto">Par de Ingressos Cinema</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$700</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/valeverde2.jpg">				  	
+					   		<h4 class="produto">Cachaça Vale Verde</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$700</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+						<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/pizza1.jpg">
+					   		<h4 class="produto">Rodízio de Pizza com Acompanhante</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$1000</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+					   		<img class="imgproduto" src="../assets/imagens/dia.jpg">
+					   		<h4 class="produto">Folga</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$1000</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					  	<div class="text-center">
+					   		<img class="imgproduto" src="../assets/imagens/jack1.jpg">
+					   		<h4 class="produto">Jack Damiels/Absolut</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$1500</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					  	<div class="text-center">
+					   		<img class="imgproduto" src="../assets/imagens/babybeef1.jpg">
+					   		<h4 class="produto">Rodízio Babe Beef com Acompanhante</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$1500</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/hometheater.jpg">				  	
+					   		<h4 class="produto">Home Theather</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$3000</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4 col-md-offset-4">
+				<div class="panel panel-default">
+					  <div class="panel-body">
+					   	<div class="text-center">
+						   	<img class="imgproduto" src="../assets/imagens/semana1.jpg">				  	
+					   		<h4 class="produto">Uma semana de folga</h4>
+					   		<img class="imgmoeda" src="../assets/avancoins.png">
+					   		<text class="preco">$5000</text>
+					   	</div>
+					  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+ <script src="../libs/jquery/js/jquery-3.2.1.min.js"></script>
+ <script src="../libs/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
