03246c966f3b6337a850b061476ddc483ee8eb05
Lucas Aguiar <de.lucas73@gmail.com>
2018-01-04 17:28:18 -0200

    fonte alterada coms sucesso

--- public/index.php
+++ public/index.php
@@ -15,12 +15,14 @@
 			<div class="col-md-12">
 				<h1 class="textotitulo">LOJA AVANÇÃO</h1>
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4">
 				<div class="panel panel-default">
 					  <div class="panel-body">
 					  	<div class="text-center">
 					  		<img class="imgproduto" src="../assets/imagens/acai1.jpg">
-					   		<h4 class="produto">Açai 300ml</h4>
+					   		<h5 class="produto">Açai 300ml</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">100</text>
 					   	 </div>
@@ -32,7 +34,7 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/acai2.jpg">
-					   		<h4 class="produto">Açai 500ml</h4>
+					   		<h5 class="produto">Açai 500ml</h5>
 							<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">150</text>
 					   	</div>
@@ -44,19 +46,21 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/ventilador1.jpg">
-					   		<h4 class="produto">Ventilador de Mesa</h4>
+					   		<h5 class="produto">Ventilador de Mesa</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">500</text>
 					   	</div>
 					  </div>
 				</div>
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4">
 				<div class="panel panel-default">
 					  <div class="panel-body">
 					   	<div class="text-center">
 					   		<img class="imgproduto" src="../assets/imagens/pendrive1.jpg">
-					   		<h4 class="produto">Pendrive 64GB</h4>
+					   		<h5 class="produto">Pendrive 64GB</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">500</text>
 					   	</div>
@@ -68,7 +72,7 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/manicure1.jpg">
-					   		<h4 class="produto">Manicure</h4>
+					   		<h5 class="produto">Manicure</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">400</text>
 					   	</div>
@@ -80,19 +84,21 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 					   		<img class="imgproduto" src="../assets/imagens/cinema1.jpg">
-					   		<h4 class="produto">Par de Ingressos Cinema</h4>
+					   		<h5 class="produto">Par de Ingressos Cinema</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">700</text>
 					   	</div>
 					  </div>
 				</div>
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4">
 				<div class="panel panel-default">
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/valeverde2.jpg">
-					   		<h4 class="produto">Cachaça Vale Verde</h4>
+					   		<h5 class="produto">Cachaça Vale Verde</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">700</text>
 					   	</div>
@@ -104,7 +110,7 @@
 					  <div class="panel-body">
 						<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/pizza1.jpg">
-					   		<h4 class="produto">Rodízio de Pizza com Acompanhante</h4>
+					   		<h5 class="produto">Rodízio de Pizza com Acompanhante</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">1000</text>
 					   	</div>
@@ -116,19 +122,21 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 					   		<img class="imgproduto" src="../assets/imagens/dia.jpg">
-					   		<h4 class="produto">Folga</h4>
+					   		<h5 class="produto">Folga</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">1000</text>
 					   	</div>
 					  </div>
 				</div>
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4">
 				<div class="panel panel-default">
 					  <div class="panel-body">
 					  	<div class="text-center">
 					   		<img class="imgproduto" src="../assets/imagens/jack1.jpg">
-					   		<h4 class="produto">Jack Damiels/Absolut</h4>
+					   		<h5 class="produto">Jack Damiels/Absolut</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">1500</text>
 					   	</div>
@@ -140,7 +148,7 @@
 					  <div class="panel-body">
 					  	<div class="text-center">
 					   		<img class="imgproduto" src="../assets/imagens/babybeef1.jpg">
-					   		<h4 class="produto">Rodízio Babe Beef com Acompanhante</h4>
+					   		<h5 class="produto">Rodízio Babe Beef com Acompanhante</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">1500</text>
 					   	</div>
@@ -152,19 +160,21 @@
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/hometheater.jpg">
-					   		<h4 class="produto">Home Theather</h4>
+					   		<h5 class="produto">Home Theather</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">3000</text>
 					   	</div>
 					  </div>
 				</div>
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
 				<div class="panel panel-default">
 					  <div class="panel-body">
 					   	<div class="text-center">
 						   	<img class="imgproduto" src="../assets/imagens/semana1.jpg">
-					   		<h4 class="produto">Uma semana de folga</h4>
+					   		<h5 class="produto">Uma semana de folga</h5>
 					   		<img class="imgmoeda" src="../assets/avancoins.png">
 					   		<text class="preco">5000</text>
 					   	</div>
